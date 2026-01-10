35a204d7fc4973b0851671b4ab1670ccb33eb621
Travis <theller5567@gmail.com>
2026-01-10 16:51:20 -0500

    New Resume

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
     const [swordRotation, setSwordRotation] = useState(0);
 
     function downloadResume(){
-        window.open('/assets/Travis-Heller-Resume-2026.pdf', '_blank');
+        window.open('/assets/Travis_Heller_Resume26.pdf', '_blank');
     }
 
     const isMobile = useMediaQuery({maxWidth: 853});
